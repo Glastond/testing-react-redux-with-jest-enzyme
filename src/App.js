f274d74475af6e7df1cef2ef360c4070b1512de1
Glaston <glaston@astrumlabs.co>
2019-12-26 14:59:41 +0530

    Initial commits of my components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Header from "./Components/header/index.jsx";
+import "./app.scss";
+
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">
+          <h3>Logo</h3>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
